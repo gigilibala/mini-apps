f8aeb11f30a66e45f93077e88741d662953e5976
Amin Hassani <ahassanni@cis.uab.edu>
2015-04-04 15:57:26 -0500

    working on checkpointing stuff

--- mantevo/minife-2.0/src/checkpoint.hpp
+++ mantevo/minife-2.0/src/checkpoint.hpp
@@ -6,20 +6,57 @@ namespace miniFE{
 	
 class Checkpoint{
 
-	Checkpoint(){/* do nothing */}
+private:
+	char* remote_checkpoint_buf[2];
+	char* my_checkpoint_buf;
+	int remote_rank;
+	
+public:	
+	Checkpoint(int max_size){
+
+
+		int remote_checkpoint_idx = 0;
+		remote_checkpoint_buf[0] = malloc(max_size);
+		remote_checkpoint_buf[1] = malloc(max_size);
+		if(NULL == remote_checkpoint_buf[0] || NULL == remote_checkpoint_buf[1]){
+			MPI_Abort(MPI_COMM_WORLD, 0);
+		}
+
+		my_checkpoint_buf[1] = malloc(max_size);
+		if(NULL == my_checkpoint_buf[0]){
+			MPI_Abort(MPI_COMM_WORLD, 0);
+		}
+		
+	}
 	~Checkpoint(){/* do nothing */}
 
+	
+	void checkpoint(){
 
+		MPI_Comm world = FTComm::get_world_comm();
+		int rank, size, remote;
+		MPI_Comm_size(world, &size);
+		MPI_Comm_rank(world, &rank);
+
+		remote = (rank + (size/2)) % size;
+
+		MPI_Irecv();
+		MPI_Isend();
+		
+	}
+
+	void restart(){
+
+	}
 	
 	template<typename MatrixType, typename VectorType, typename Scalar>
 	int checkpoint_write(int k, Scalar rtrans, Scalar oldrtrans, 
 						 VectorType& p,
 						 VectorType& r,
-						 MatrixType& A,
-						 char* buffer, int buf_size){
+						 MatrixType& A){
 
 		int tmp_size;
-		char* buf = buffer;
+		char* buf = my_checkpoint_buf;
 		int size = 0; 
 		size += A.checkpoint_write(NULL, 0) +
 			p.checkpoint_write(NULL, 0) +
@@ -39,6 +76,9 @@ class Checkpoint{
 		buf += r.checkpoint_write(buf, buf_size);
 
 		assert(buf - buffer == size);
+
+		send_checkpoint();
+		
 		return size;
 	}
 	
@@ -47,11 +87,10 @@ class Checkpoint{
 	int checkpoint_read(int& k, Scalar& rtrans, Scalar& oldrtrans, 
 						VectorType& p,
 						VectorType& r,
-						MatrixType& A,
-						char* buffer, int buf_size){
+						MatrixType& A){
 		
 		int tmp_size;
-		char* buf = buffer;
+		char* buf = my_checkpoint_buf;
 
 		if(buf == NULL)
 			return 0;
